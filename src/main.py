817c0381aa3a966711722fa2877803297a2296ce
Ubuntu <ubuntu@ip-172-31-35-142.ec2.internal>
2025-01-29 21:55:04 +0000

    Commit from AWS!

--- src/main.py
+++ src/main.py
@@ -3,6 +3,8 @@ from preprocessing import split_data
 from model import train_model
 from evaluation import evaluate_model
 
+print("Added from AWS! - Peter")
+
 data = load_data()
 X_train, X_test, y_train, y_test = split_data(data)
 model = train_model(X_train, y_train)
