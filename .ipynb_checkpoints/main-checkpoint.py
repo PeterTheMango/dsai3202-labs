e36d237a1c63d85ccc297606455f8070dde807df
Peter Yeshua Sotomango <pyeshuajs23@gmail.com>
2025-03-05 09:01:11 +0300

    Uploaded performance metrics

--- .ipynb_checkpoints/main-checkpoint.py
+++ .ipynb_checkpoints/main-checkpoint.py
@@ -1,50 +1,44 @@
-from src import process,sequential,threading,cleaning
-import pandas as pd
+import glob
+import multiprocessing
+from src.functions import read_images
+from src.sequential import run_sequential
+from src.parallel import run_parallel
+from src.features import extract_feature
+from src.splitting import split_data
+from src.train_test import train_models, test_models
+# Define the path to the dataset
+dataset_path = 'data/brain_tumor_dataset/'
 
-sequentialTime, seq_total = sequential.run_sequentially(test_cases)
-parallelTime, parallel_speedup, par_total = threading.run_threads(test_cases)
-processTime, process_speedup, proc_total = process.run_processes(test_cases)
+# List all image files in the 'yes' and 'no' directories
+yes_images = glob.glob(dataset_path + 'yes/*.jpg')
+no_images = glob.glob(dataset_path + 'no/*.jpg')
 
-print(f"=========================== Total Calculated ===========================")
-print(f"Sequential: {seq_total}")
-print(f"Parallel: {par_total}")
-print(f"Multiprocessing: {proc_total}")
+yes_images = read_images(yes_images)
+no_images = read_images(no_images)
 
-print(f"Time analysis done according to {test_cases} items.")
+print(f"Number of 'yes' images: {len(yes_images)}")
+print(f"Number of 'no' images: {len(no_images)}")
 
-print(f"=========================== Sequential Time Analysis ===========================")
-print(f"Time it took: {sequentialTime} seconds.")
+# Sequential Running
+seq_time = run_sequential(yes_images, no_images)
+print(f"Sequential Running: {seq_time}s")
 
-print(f"=========================== Parallel Time Analysis ===========================")
-print(f"Time it took: {parallelTime} seconds.")
+# Parallel 
+par_time, yes_inputs, no_inputs = run_parallel(yes_images, no_images)
+print(f"Parallel Running: {par_time}")
 
-print(f"=========================== Multi Processing Time Analysis ===========================")
-print(f"Time it took: {processTime} seconds.")
+num_cpu = multiprocessing.cpu_count() 
+speedup = seq_time / par_time
+efficiency = speedup / num_cpu
 
-par_seq_speedup = sequentialTime/parallelTime
-seq_prc_speedup = sequentialTime/processTime
-par_mlt_speedup = parallelTime/processTime
+print(f"Speedup: {speedup:.2f}x")
+print(f"Efficiency: {efficiency:.2f}")
 
-print(f"=========================== Speedup Analysis ===========================")
-print(f"Speedup from sequential to parallel: {par_seq_speedup}")
-print(f"Speedup from sequential to multiprocess: {seq_prc_speedup}")
-print(f"Speedup from parallel to multiprocess: {par_mlt_speedup}")
+data, running_time = extract_feature(yes_inputs, no_inputs)
+X_train, X_test, y_train, y_test = split_data(data)
 
-cores = cpu_count()
+models = train_models(X_train, y_train)
 
-eff_seq_par = par_seq_speedup / cores
-eff_seq_mlt = seq_prc_speedup / cores
-eff_par_mlt = par_mlt_speedup / cores
+for name, model in models.items():
+    test_models(name, model, X_test, y_test)
 
-print(f"=========================== Efficiency Analysis ===========================")
-print(f"Efficiency of sequential to parallel: {eff_seq_par}")
-print(f"Efficiency of sequential to multiprocess: {eff_seq_mlt}")
-print(f"Efficiency of parallel to multiprocess: {eff_par_mlt}")
-
-print(f"=========================== Amdahl Speedup Analysis ===========================")
-print(f"Amdahl Parallel Speedup: {parallel_speedup}")
-print(f"Amdahl Process Speedup: {process_speedup}")
-
-print(f"=========================== Gustafsson Speedup Analysis ===========================")
-print(f"Gustafsson Parallel Speedup: {cores - (1- par_seq_speedup) * (cores - 1)}")
-print(f"Gustafsson Process Speedup: {cores - (1- seq_prc_speedup) * (cores - 1)}")
